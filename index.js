51cd727ca8e1c54cc4ea72e72876cea98851524a
Julia <jdaianova@gmail.com>
2022-07-13 15:13:57 +0200

    Revert "1 commit"
    This reverts commit 7d9858788a9f2f58574ea1702da27e99c844c7ad.

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-const searchElement = document.querySelector('#search');
-const shows = document.querySelectorAll('.show');
+const btnBack = document.querySelector('#back');
+const btnNext = document.querySelector('#next');
+const photos = [
+  'img/italia_1.jpg',
+  'img/italia_2.jpg',
+  'img/italia_3.jpg',
+  'img/italia_4.jpg',
+  'img/italia_5.jpg',
+  'img/italia_6.jpg',
+];
 
-searchElement.addEventListener('keyup', (event) => {
-    const inputWord = event.target.value.toLowerCase();
+i = 0;
 
-    shows.forEach( (item) => {
-        item.querySelector('p').textContent.toLowerCase().includes(inputWord) ? item.style.display = 'block' : item.style.display = 'none';
-    });
-
+btnNext.addEventListener('click', () => {
+  i++;
+  if (i > photos.length-1) {
+    i = 0;
+  };
+  document.querySelector('#photo').src = photos[i];
 });
+
+btnBack.addEventListener('click', () => {
+  i--;
+  if (i < 0) {
+    i = photos.length-1;
+  };
+  document.querySelector('#photo').src = photos[i];
+});
+
+//console.log(photos);
